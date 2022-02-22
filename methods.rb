49b30d589e9ec329b6d7ba6cc34acf3b5bce8f2c
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-02-22 14:58:40 -0800

    commit tree 501dbd672e12722598623a052ebb5c668b467d8b

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+
+
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(x, y)
+    x + y
+end
+
+def halve(n)
+    return nil unless n.class == Integer
+
+    n/2
+end
+
